354f8de6b5b7ca4144e4435d48c10fa94e006976
Tim Meusel <tmu@hosteurope.de>
2014-11-05 16:14:32 +0100

    fixed typo

--- manifests/unicorn.pp
+++ manifests/unicorn.pp
@@ -1,5 +1,5 @@
 # this class installs nginx with unicorn infront of puppetmaster
-class puppet:unicorn () {
+class puppet::unicorn () {
   include nginx
 #  nginx::resource::vhost {'puppetmaster':
 #    www_root => '/var/empty',
