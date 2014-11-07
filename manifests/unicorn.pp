e4ad481d3f5cab169e91f2d7b5172d68a2aaae1b
Tim Meusel <tmu@hosteurope.de>
2014-11-07 14:54:05 +0100

    add support for nginx/unicorn
    modified puppet::master class for using $webserver.
    this can be httpd for apache/passanger or nginx for nginx/unicorn.
    nginx is currently only tested on centos7, but should work on evers OS with systemd
    
    the nginx vhost definition is currently a simple file ressource, this should be changed
    to nginx::resource:vhost in the future

--- manifests/unicorn.pp
+++ manifests/unicorn.pp
@@ -1,7 +1,54 @@
-# this class installs nginx with unicorn infront of puppetmaster
+# this class installs nginx with unicorn in front of puppetmaster
+# tested only on centos 7
+
 class puppet::unicorn () {
   include nginx
-#  nginx::resource::vhost {'puppetmaster':
-#    www_root => '/var/empty',
-#  }
+  # install unicorn
+  package {['unicorn', 'rack']:
+    ensure    => 'latest',
+    provider  => 'gem',
+  } ->
+  file {'copy-config':
+    path    => '/etc/puppet/config.ru',
+    source  => '/usr/share/puppet/ext/rack/config.ru',
+  } ->
+  file {'unicorn-conf':
+    path    => '/etc/puppet/unicorn.conf',
+    source  => 'puppet:///modules/puppet/unicorn.conf',
+    
+  } ->
+  file {'unicorn-service':
+    path    => '/usr/lib/systemd/system/unicorn-puppetmaster.service',
+    source  => 'puppet:///modules/puppet/unicorn-puppetmaster.service',
+    notify  => Exec['systemd-reload'],
+  }
+  exec{'systemd-reload':
+    exec        => 'systemctl daemon-reload',
+    refreshonly => 'true',
+    notify      => Service['unicorn-puppetmaster'],
+  }
+  unless defined(Service['unicorn-puppetmaster']) {
+    service{'unicorn-puppetmaster':
+      ensure  => 'running',
+      enable  => 'enable',
+    }
+  }
+  # hacky vhost
+  file {'puppetmaster-vhost':
+    path    => '/etc/nginx/sites-available/puppetmaster',
+    source  => 'puppet:///puppet/puppetmaster',
+  } ->
+  file {'enable-puppetmaster-vhost':
+    path    => '/etc/nginx/sites-enabled/puppetmaster',
+    ensure  => 'link',
+    target  => '/etc/nginx/sites-available/puppetmaster',
+    notify  => Service['nginx'],
+  }
+  unless defined(Service['nginx']) {
+    service{'nginx':
+      ensure  => 'running',
+      enable  => 'enable',
+    }
+  }
 }
+
