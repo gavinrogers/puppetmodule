c15ec4a31113499d2dee89a231a264a26c34bfa2
Tim Meusel <tmu@hosteurope.de>
2015-01-22 15:21:44 +0100

    add missing param unicorn_package

--- manifests/master.pp
+++ manifests/master.pp
@@ -36,6 +36,7 @@
 #  ['listen_address']           - IP for binding the webserver, defaults to *
 #  ['disable_ssl']              - Disables SSL on the webserver. usefull if you use this master behind a loadbalancer. currently only supported by nginx, defaults to undef
 #  ['backup_upstream']          - specify another puppet master as fallback. currently only supported by nginx
+#  ['unicorn_package']          - package name of a unicorn rpm. if provided we install it, otherwise we built it via gem/gcc
 #  ['unicorn_path']             - custom path to the unicorn binary
 #
 # Requires:
@@ -93,6 +94,7 @@ class puppet::master (
   $disable_ssl                = $::puppet::params::disable_ssl,
   $backup_upstream            = $::puppet::params::backup_upstream,
   $unicorn_path               = $::puppet::params::unicorn_path,
+  $unicorn_package            = $::puppet::params::unicorn_package,
 ) inherits puppet::params {
 
   anchor { 'puppet::master::begin': }
